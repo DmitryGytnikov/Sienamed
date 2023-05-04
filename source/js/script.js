84f1cf5b438ccebbef879910f6292e831124b0c3
DmitryGytnikov <gytnikovd@mail.ru>
2023-05-04 16:19:17 +0300

    Add layout of page (desktop):
    about.html

--- source/js/script.js
+++ source/js/script.js
@@ -14,3 +14,22 @@ searchClose.addEventListener(`click`, function (evt) {
   evt.preventDefault();
   headerSearchBurger.classList.remove(`page-search--active`);
 });
+
+// $(function () {
+
+  // Cлайдер main__about-slider на main.html
+  // let slider2 = document.querySelector(`.main__about-slider`);
+  // if (slider2) {
+  //   $('.main__about-slider').slick({
+  //     dots: true,
+  //     fade: true,
+  //     autoplay: true,
+  //     autoplaySpeed: 4000,
+  //     slidesToShow: 1,
+  //     prevArrow: $('.presentation__button-slider--left-1'),
+  //     nextArrow: $('.presentation__button-slider--right-1'),
+  //   });
+  // }
+
+
+// });
