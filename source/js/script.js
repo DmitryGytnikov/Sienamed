6d51e798920649b796b54aaf48a46f14585b0615
DmitryGytnikov <gytnikovd@mail.ru>
2023-05-20 17:05:51 +0300

    Add layout of page (desktop):
    main.html (remaining blocks)

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,12 @@
 'use strict';
 
-// Выпадающее окно поиска
+// Выпадающее окно поиска (кнопки .header-top__search-burger в хедере и
+// .header-top__search-burger--services на главной странице)
 let burger = document.querySelector(`.header-top__search-burger`);
 let headerSearchBurger = document.querySelector(`.page-search`);
 let searchClose = document.querySelector(`.page-search__close`);
+
+let burgerServices = document.querySelector(`.header-top__search-burger--services`);
 
 burger.addEventListener(`click`, function (evt) {
   evt.preventDefault();
@@ -15,6 +18,12 @@ searchClose.addEventListener(`click`, function (evt) {
   headerSearchBurger.classList.remove(`page-search--active`);
 });
 
+burgerServices.addEventListener(`click`, function (evt) {
+  evt.preventDefault();
+  headerSearchBurger.classList.add(`page-search--active`);
+});
+
+
 $(function () {
 
   // Главный слайдер на main.html
@@ -23,7 +32,7 @@ $(function () {
     $('.slider-big').slick({
       dots: false,
       // fade: true,
-      // autoplay: true,
+      autoplay: true,
       autoplaySpeed: 4000,
       slidesToShow: 1,
       prevArrow: $('.slider-big__button--left'),
@@ -74,6 +83,57 @@ $(function () {
   }
 
 
+  // Cлайдер slider-specialists на main.html
+  let slider3 = document.querySelector(`.slider-reviews`);
+  if (slider3) {
+    $('.slider-reviews').slick({
+      dots: false,
+      arrows: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      prevArrow: $('.slider-reviews__button--left'),
+      nextArrow: $('.slider-reviews__button--right'),
 
+      // responsive: [{
+      //   breakpoint: 1377,
+      //   settings: {
+      //     slidesToShow: 3,
+      //   }
+      // }, {
+      //   breakpoint: 1090,
+      //   settings: {
+      //     slidesToShow: 2,
+      //   }
+      // }, {
+      //   breakpoint: 740,
+      //   settings: {
+      //     slidesToShow: 2,
+      //   }
+      // }, {
+      //   breakpoint: 661,
+      //   settings: {
+      //     slidesToShow: 2,
+      //     centerMode: false,
+      //   }
+      // }, {
+      //   breakpoint: 496,
+      //   settings: {
+      //     slidesToShow: 3,
+      //     centerMode: false,
+      //   }
+      // }]
+    });
+  }
+
+
+  // Табы ("Характеристики") на catalog-item-1.html
+  $('.tab-1').click(function(e) {
+    e.preventDefault();
+    $($(this).closest('.basic__services-nav').find('li').find('.tab-1')).removeClass('tab--active-1');
+    $($(this).closest('.basic__services').find('.basic-services__content-wr').find('.basic-services__content-list')).removeClass('tabs-content--active-1');
+
+    $(this).addClass('tab--active-1');
+    $($(this).attr('href')).addClass('tabs-content--active-1');
+  });
 
 });
